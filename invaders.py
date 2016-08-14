5567fbc7e0bbd0f07b1c0bd60dc09d6b81eed028
Zeth <zeth@infomachine.uk>
2016-08-14 15:41:57 +0100

    Save a bit of RAM by commenting out the comments.

--- invaders.py
+++ invaders.py
@@ -1,10 +1,10 @@
-"""MicroPython Invaders by Zeth"""
+#"""MicroPython Invaders by Zeth"""
 
 # pylint: disable=import-error,too-many-instance-attributes
 from microbit import display, Image, button_a, button_b, sleep
 
 def row_generator(x_pos=2):
-    """Make a single row of the cannon."""
+    #"""Make a single row of the cannon."""
     i = 0
     while i != 5:
         if i == x_pos:
@@ -14,7 +14,7 @@ def row_generator(x_pos=2):
         i = i + 1
 
 class Game:
-    """Save the world from invading aliens!"""
+    #"""Save the world from invading aliens!"""
     def __init__(self):
         self.score = 0
         self.sky = ""
@@ -31,18 +31,18 @@ class Game:
         self.update_cannon()
 
     def clear_sky(self):
-        """Create an empty sky."""
+        #"""Create an empty sky."""
         self.sky_matrix = ["00000" for i in range(3)]
 
     def drop_invaders(self):
-        """Drop the invaders down a line."""
+        #"""Drop the invaders down a line."""
         bottom = self.sky_matrix.pop()
         if '8' in bottom:
             return -1
         self.insert_row()
 
     def move_cannon(self):
-        """Move the cannon one LED across."""
+        #"""Move the cannon one LED across."""
         if self.cannon_direction == 1 and self.cannon_x_pos < 4:
             self.cannon_x_pos += 1
         elif self.cannon_direction == 1 and self.cannon_x_pos == 4:
@@ -55,27 +55,27 @@ class Game:
             self.cannon_x_pos -= 1
 
     def update_cannon(self):
-        """Update the latest state of the bottom row."""
+        #"""Update the latest state of the bottom row."""
         self.cannon_row = ''.join(row_generator(self.cannon_x_pos))
 
     def update_sky(self):
-        """Convert the sky matrix to a string."""
+        #"""Convert the sky matrix to a string."""
         self.sky = ':'.join(self.sky_matrix) + ':'
 
     def show(self):
-        """Update the game screen."""
+        #"""Update the game screen."""
         image = Image(self.sky + self.cannon_row)
         display.show(image)
 
     def fire(self):
-        """Fire the cannon."""
+        #"""Fire the cannon."""
         if (self.bullet_x, self.bullet_y) == (0, -1):
             self.bullet_x = self.cannon_x_pos
             self.bullet_y = 3
             self.update_bullet_cell("9")
 
     def update_bullet_cell(self, char):
-        """Update the bullet cell."""
+        #"""Update the bullet cell."""
         out_str = ""
         for indi, value in enumerate(self.sky_matrix[self.bullet_y]):
             if indi == self.bullet_x:
@@ -85,7 +85,7 @@ class Game:
         self.sky_matrix[self.bullet_y] = out_str
 
     def update_bullet(self):
-        """Move the bullet."""
+        #"""Move the bullet."""
         if self.bullet_y != -1:
             self.update_bullet_cell("0")
             self.bullet_y -= 1
@@ -102,7 +102,7 @@ class Game:
                     self.score += 1
 
     def invaders_move(self):
-        """Have them fly back and forth."""
+        #"""Have them fly back and forth."""
         if self.invader_direction == 1:
             if all((i[4] == "0" for i in self.sky_matrix)):
                 self.sky_matrix = [("0" + i[0:-1]) for i in self.sky_matrix]
@@ -115,11 +115,11 @@ class Game:
                 self.invader_direction = 1
 
     def insert_row(self):
-        """Insert a new row."""
+        #"""Insert a new row."""
         self.sky_matrix.insert(0, "88880")
 
     def run(self):
-        """Run the game."""
+        #"""Run the game."""
         i = 0
         wave = 200
         wiggle = 80
@@ -144,7 +144,7 @@ class Game:
                     wiggle -= 5
 
 def main():
-    """Run on start."""
+    #"""Run on start."""
     game = Game()
     game.run()
     score_str = str(game.score)
